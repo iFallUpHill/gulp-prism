13581db45bc12ca6ab39b8a951e56227cd8c6c1e
Jason Manson-Hing <jason.mansonhing@gmail.com>
2017-12-31 03:11:50 -0500

    Initial commit (Working)

---
+++ README.md
@@ -0,0 +1,62 @@
+# gulp-prism
+
+[![npm](https://img.shields.io/npm/v/gulp-prism.svg)]()
+[![npm](https://img.shields.io/npm/l/gulp-prism.svg)]()
+
+[Check it out on npm!](https://www.npmjs.com/package/gulp-prism "gulp-prism")
+
+## About
+Pipe code snippets through [Prism](http://prismjs.com/ "Prism") using `gulp` to pre-tag code blocks at compile time. This only applies to code blocks formatted properly using both the `pre` and `code` tags (i.e. `<pre><code>`) and that have a specified `{lang|language}-{LANG_NAME}` class on the `code` tag. See the usage section for more information. 
+
+## Install
+
+```bash
+npm install gulp-prism --save-dev
+```
+
+## Usage
+
+### gulpfile.js
+```js
+var gulp = require('gulp');
+var highlight = require('gulp-prism');
+
+gulp.task('default', function () {
+	gulp.src('src/index.html')
+		.pipe(highlight())
+		.pipe(gulp.dest('dist'));
+});
+```
+
+### src/index.html
+
+#### Input 
+
+```html
+<pre><code class="language-html">&lt;div&gt;
+	This will be highlighted!
+&lt;div&gt;</code></pre>
+
+```
+
+#### Output
+```
+<pre><code class="language-html prism"><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span><span class="token punctuation">&gt;</span></span>
+    This will be highlighted!
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span><span class="token punctuation">&gt;</span></span></code></pre>
+```
+
+#### Notes
+
+* You must specify the snippet language on the `<code>` tag or else the code block will not be highlighted.
+
+* You must use escaped HTML within the `<pre><code>` block.
+
+* Code blocks that have been highlighted using Prism will have the `prism` class added to them. Similarly, code blocks that were not highlighted will have the `no-prism` class added to them.
+
+## Known Issues
+
+* No CSS is included. This is intentional as this library is only intended to pre-wrap code snippets within code blocks using Prism's semantic tags. You are required to define and include your own CSS.
+
+* Plugins are currently not supported. Feel free to submit a pull request to help get the ball moving!
+
---
+++ index.js
@@ -0,0 +1,104 @@
+'use strict'
+
+const cheerio       = require('cheerio');
+const PluginError   = require('plugin-error');
+const through       = require('through2');
+const path          = require('path');
+const fs            = require('fs');
+
+const langPath = "node_modules/prismjs/";
+const normalizedPath = path.join(__dirname, langPath);
+const brokenLanguages = ['django'];
+
+function loadAllLanguages() {
+    let languagesToLoad = [];
+    fs.readdirSync(normalizedPath + "components/").forEach( file => {
+        if (/^.+\.min\.(js)$/i.test(file)) {
+            let languageName = file.replace('prism-', '').replace('.min.js', '');
+            if (languageName !== 'core' && brokenLanguages.indexOf(languageName) === -1) {
+                languagesToLoad.push(languageName);
+            }
+        }
+    });
+    return languagesToLoad;
+}
+
+const PrismLoader = require(normalizedPath + "tests/helper/prism-loader");
+const Prism = PrismLoader.createInstance(brokenLanguages.concat(loadAllLanguages()));
+
+const packageName   = require('./package.json').name;
+const defaultConfig = {
+    selector: 'pre code',
+    cheerio: {
+        decodeEntities: false,
+    }
+}
+
+const entityMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;',
+    '/': '&#x2F;',
+    '`': '&#x60;',
+    '=': '&#x3D;'
+}
+
+function escapeHTML (HTMLString) {
+    return String(HTMLString).replace(/[&<>"'`=\/]/g, s => entityMap[s]);
+}
+
+function highlight (text, config) {
+    const $ = cheerio.load(text, config.cheerio);
+
+    $(config.selector)
+        .each((i, el) => {
+            const langPrefixRegex = /\blang(?:uage)?-([\w-]+)\b/i;
+            const blockClasses = $(el).attr('class') ? $(el).attr('class').split(' ') : [];
+            let hasLangClass = false;
+            let language = '';
+
+            for (let i = 0; i < blockClasses.length; i++) {
+                let match = blockClasses[i].match(langPrefixRegex);
+                if (match) {
+                    hasLangClass = true;
+                    language = match[1].toLowerCase();
+                }
+            }
+
+            const prismLangObj = Prism.languages[language];
+
+            if(hasLangClass && language && prismLangObj) {
+                $(el).text(Prism.highlight($(el).html(), prismLangObj)).addClass('prism');
+            } else {
+                $(el).text(escapeHTML($(el).text())).addClass('no-prism');
+            }
+        });
+
+    return $.html() || text;
+}
+
+module.exports = (options) => {
+    const config = Object.assign({}, defaultConfig, options)
+
+    return through.obj(function (file, encoding, done) {
+        if (file.isNull()) {
+            return done(null, file);
+        }
+
+        if (file.isStream()) {
+            this.emit('error', new PluginError(packageName, 'Streams not supported!'));
+            return done();
+        }
+
+        try {
+            const text = file.contents.toString();
+            file.contents = Buffer.from(highlight(text, config));
+        } catch (error) {
+            this.emit('error', new PluginError(packageName, error));
+        }
+
+        done(null, file);
+    })
+}
